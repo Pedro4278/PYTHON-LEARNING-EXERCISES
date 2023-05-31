50b927c7bb460b2747b11ccb839ef7e0fd6b2c96
Pedro4278 <130411439+Pedro4278@users.noreply.github.com>
2023-05-31 14:25:33 -0300

    Add files via upload

---
+++ EX.29.py
@@ -0,0 +1,9 @@
+#com este programa você consegue ler se o numero é impar ou par
+
+v=int(input('o numero aqui '))
+
+if v % 2 == 0:
+    print('O numero {} este numero é par.'.format(v))
+else:
+    print('O numero {} é impar.')
+    
