b246863eff278a82e6c6f825d809f5856e2ff1d5
Pedro4278 <130411439+Pedro4278@users.noreply.github.com>
2023-05-31 14:27:03 -0300

    Add files via upload

---
+++ aula 07 ex. 3.py
@@ -0,0 +1,4 @@
+n1 =int(input('Digite a nota do primeiro bimestre aqui: '))
+n2 =int(input('Digite a nota do segundo bimestre aqui: '))
+re =(n1+ n2)/2
+print('A media do seu aluno no primeiro semestre foi:{}'.format(re))
