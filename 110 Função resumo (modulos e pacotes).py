0edd792668863653acdd79b65e034c3c2a156667
Pedro4278 <130411439+Pedro4278@users.noreply.github.com>
2023-07-13 17:16:42 -0300

    Add files via upload
    Exercício de treinamento em Python

---
+++ 110 Função resumo (modulos e pacotes).py
@@ -0,0 +1,9 @@
+# Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(),
+# que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
+
+import moeda_curso
+
+
+valor=int(input('Digite o valor: '))
+
+print(moeda_curso.resumo(valor))
